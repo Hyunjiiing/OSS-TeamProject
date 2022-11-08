0904cd223e06516b687b162ed07290440203c3ba
troymerai <16alexandros57@gmail.com>
2022-11-09 00:00:15 +0900

    FCM 푸시 알림기능 추가

--- troymerai/flutter UI/prototype/keunalarm/lib/main.dart
+++ troymerai/flutter UI/prototype/keunalarm/lib/main.dart
@@ -1,10 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:keunalarm/firebase_options.dart';
 import 'package:keunalarm/src/binding/init_bindings.dart';
+import 'package:keunalarm/src/controller/notification_controller.dart';
 
 import 'src/app.dart';
 
-void main() {
+/** 앱이 백그라운드 상태일 때 수신되는 메시지 관리 */
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message : ${message.messageId}");
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message wilst in the foreground!');
+    print('Message data : ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification : ${message.notification}');
+    }
+  });
+
   runApp(const MyApp());
 }
 
