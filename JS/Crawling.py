3e35760b7296d74558736dc73ef1ef8faf58c155
jithhuP <107339114+jithhuP@users.noreply.github.com>
2022-11-21 11:02:48 +0900

    2022-11-21
    분할 URL과 title 및 URL 페이지 크롤링 코드
    아직 못한거 : firebase 및 날짜

--- JS/Crawling.py
+++ JS/Crawling.py
@@ -1,16 +1,31 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
+
 i = 1
 
 html = urlopen("https://www.chungbuk.ac.kr/site/www/boardList.do?boardSeq=112&key=698")
 bs0bject = BeautifulSoup(html, "html.parser")
 
-with open("C:\study\Test.txt", "w") as file:
-    for anchor in bs0bject.select("td.subject"):
-        URL = (str(i) + str(" : ") + anchor.text.strip(), anchor.a['href'])
+with open("E:\Study_folder\Test.txt", "w") as file:
+    for anchor in bs0bject.body.select("td.subject"):
+        Text = (str(i) + str(": ") + anchor.text.strip())
+        URL = (anchor.a['href'].strip())
+        #Writer = (bs0bject.tbody.select("span", {"style":"white-space:nowrap;"}).strip())
+       ## re_URL = URL.repalce("a","b",10) #문자열 값 제거하는 건데 이미 strip를 사용하여 쓸모가 없음
         i = i + 1
-        file.write(str(URL)+str("\n"))
+        print(str(Text) + str("\n") + str(URL))
+        file.write(str(Text) + str("\n") + str(URL))
 
+html = urlopen("https://www.chungbuk.ac.kr/site/www/boardView.do?post=3210602&page=&boardSeq=112&key=698")
+bs0bject = BeautifulSoup(html, "html.parser")
+
+with open("E:\Study_folder\Test2.txt", "w") as file:
+    for Date in bs0bject.body.find("span",{"class":"board_dot small"}):
+        print(Date.text.strip())
+
+    for meta in bs0bject.body.select("td",{"colspan":"2"}):
+        print(meta.text.strip())
 
 
 
