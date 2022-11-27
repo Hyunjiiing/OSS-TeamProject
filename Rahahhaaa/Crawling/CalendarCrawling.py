2124de245a217e1e67059e223e6ca815ea0ef7d2
Rahahhaaa <airlike7734@gmail.com>
2022-11-28 03:49:45 +0900

    change data type of date to datetime

--- Rahahhaaa/Crawling/CalendarCrawling.py
+++ Rahahhaaa/Crawling/CalendarCrawling.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 import datetime
 date_format = '%Y.%m.%d.'
 
-res_undergraduate = {'start_date' : [], 'content' : []}
-res_graduate = {'start_date' : [], 'content' : []}
+res_undergraduate_date = []
+res_graduate_date = []
+res_undergraduate_content = []
+res_graduate_content = []
 
 def slicing(s, mode):
-    res_undergraduate['content'].append(s)
+    content = s[:]
     s = s.split(']')
     s[0] = s[0].split('~')
     start_date = s[0][0].split('[')[1].split('(')[0]
@@ -20,21 +22,20 @@ def slicing(s, mode):
             
         elif len(start_date) <= 5:
             start_date = '2022.0' + start_date
-        print(start_date)
         dt = datetime.datetime.strptime(start_date,date_format)
-        res_undergraduate['start_date'].append(dt)
+        res_undergraduate_date.append(dt)
+        res_undergraduate_content.append(content)
         
     else:
-        res_graduate['content'].append(s)
         if len(start_date) == 6:
             start_date = '2022.' + start_date
             if start_date[5] != 0:
                 start_date = start_date[:5] + '0' + start_date[6:]
         elif len(start_date) <= 5:
             start_date = '2022.0' + start_date
-        print(start_date)
         dt = datetime.datetime.strptime(start_date,date_format)
-        res_graduate['start_date'].append(dt)
+        res_graduate_date.append(dt)
+        res_graduate_content.append(content)
         
     
 
@@ -57,8 +58,6 @@ for item in data:
         elif text != '':
             slicing(text, mode)
 
-print(res_undergraduate['start_date'][0])
-print(res_undergraduate['content'][0])
 
 #파이어베이스 연동
 import firebase_admin
@@ -69,12 +68,10 @@ cred = credentials.Certificate("")
 firebase_admin.initialize_app(cred)
 firebase_db = firestore.client()
 
-for i in range(len(res_undergraduate['start_date'])):
+for i in range(len(res_undergraduate_date)):
     doc = firebase_db.collection('Calendar_undergraduate').document(u'undergraduate%d'%i)
-    doc.set({"start_date":res_undergraduate['start_date'][i],
-             "content":res_undergraduate['content'][i]})
+    doc.set({"content":res_undergraduate_content[i],"start_date":res_undergraduate_date[i]})
 
-for i in range(len(res_graduate['start_date'])):
+for i in range(len(res_graduate_date)):
     doc = firebase_db.collection('Calendar_graduate').document(u'graduate%d'%i)
-    doc.set({"start_date":res_graduate['start_date'][i],
-             "content":res_graduate['content'][i]})
+    doc.set({"content":res_graduate_content[i],"start_date":res_graduate_date[i]})
