6e8d8c468e701a834392a0368bb171aca5beb479
Soyul_Kim <todayfcn@gmail.com>
2022-12-01 04:02:27 +0900

    firebase에 ECE 공지사항 data 업로드(complete)

--- Soyul/Crawling_ECE.py
+++ Soyul/Crawling_ECE.py
@@ -3,17 +3,21 @@ import requests
 import json
 import sys
 import io
-import re #정규표현식으로 없애고 dict에 넣어 #탭 없으면 그대로니까 경우 나눌 필요 없다.
+import re #정규표현식으로 없애고 dict에 넣자 #탭이 있는 것과 없는 것. 경우 나눠야 하는가? #탭 없으면 그대로니까 경우 나눌 필요 없다.
 
+#인코딩(한글 깨짐 문제 해결)
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 
+#data 담을 dictionary 선언
 result={'title':[], 'date':[], 'link':[]}
 
+#탭 제거하는 함수(정규표현식 사용)
 def clean_text(text):
     cleaned_text = re.sub(r'[\t]', '', text)
     return cleaned_text
 
+#크롤링한 data를 dictionary에 append
 for page_num in range(3):
     url=f'https://ece.cbnu.ac.kr/index.php?mid=ece0602&page={page_num+1}' 
     res=requests.get(url) #목표로 하는 웹페이지의 html을 requests 패키지를 이용하며 받아 옴
@@ -26,7 +30,9 @@ for page_num in range(3):
         title_semi=i.a.get_text()
         title_real=clean_text(title_semi)
         url=i.a["href"]
-        result['title'].append(title_real) #'특수문자 제거하는 법' 검색
+        result['title'].append(title_real) 
+        #Problem: title 앞에 탭이 함께 출력되는 경우 존재 
+        #Solution: '특수문자 제거하는 법' 검색하기 => 정규표현식 사용해 clean_text 함수 만들었다.
         result['link'].append(url)
 
     time=soup.find_all("td",attrs={"class":"time"})
@@ -36,13 +42,13 @@ for page_num in range(3):
         result['date'].append(date_semi)
 
 
+#firebase 연동 및 data 업로드
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import firestore
 
-
-cred = credentials.Certificate('keunalarm-sample-1-firebase-adminsdk-pp1xh-9ce545d345.json')
+cred = credentials.Certificate("C:/firebase/key.json") #[Errno 2] No such file or directory => 이 부분에서 이러한 에러 발생
 firebase_admin.initialize_app(cred)
 
 firebase_database = firestore.client()
@@ -50,8 +56,8 @@ firebase_database = firestore.client()
 for i in range(len(result['title'])):
     document=firebase_database.collection('ECE_notice').document('no.%d'%i)
     document.set({
-        "date":result['title'][i],
-        "link":result['link'],
-        "date":result['date']
+        "title":result['title'][i],
+        "link":result['link'][i],
+        "date":result['date'][i],
     })
 
