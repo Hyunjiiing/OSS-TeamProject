424f83f194322b370beeabf2d658b2b8f10ef794
Soyul_Kim <todayfcn@gmail.com>
2022-11-30 15:58:24 +0900

    ECE 공지사항 title, date, link 정보 list에 넣고 출력하기

--- Soyul/Crawling_ECE.py
+++ Soyul/Crawling_ECE.py
@@ -4,57 +4,27 @@ import json
 from firebase_admin import credentials
 import sys
 import io
-import re
 
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 
-result={'title':[], 'date':[], 'link':[]}
+# result={'title':[], 'date':[], 'link':[]}
 
-def clean_text(text):
-    cleaned_text = re.sub('[a-zA-Z]', '', text)
-    cleaned_text = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]',
-                          '', cleaned_text)
-    return cleaned_text
+url='https://ece.cbnu.ac.kr/index.php?mid=ece0602&page=1' 
 
-result_title = []
-result_time = []
+res=requests.get(url) #목표로 하는 웹페이지의 html을 requests 패키지를 이용하며 받아 옴
 
-special_char = '\n\t'
-for page in range(24):
-    url = f"https://ece.cbnu.ac.kr/index.php?mid=ece0602&page={page+1}"
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    for content in soup.select('td > a'):
-        # print(content.getText('title'))
-        result_title.append(content.getText('title').replace('\t\n', ''))
+soup=bs(res.text,"lxml") #가져온 html 문서 전체를 beautifulsoup4 패키지를 이용하여 파싱(parsing)함
 
-    for content in soup.select('td.time'):
-        # print(content.getText('16:24'))
-        result_time.append(content.getText('16:24'))
+ece=soup.find_all("td",attrs={"class":"title"})
 
+for i in ece:
+    title=i.a.get_text()
+    link=i.a["href"]
+    print(title, link)
 
-print(result_time)
-print(result_title)
+time=soup.find_all("td",attrs={"class":"time"})
 
-# t=[]
-# d=[]
-# l=[]
-# hrefs=[]
-
-# url='https://ece.cbnu.ac.kr/index.php?mid=ece0602&page=1' 
-
-# response=requests.get(url) #목표로 하는 웹페이지의 html을 requests 패키지를 이용하며 받아 옴
-
-# html=bs(response.text,'html.parser') #가져온 html 문서 전체를 beautifulsoup4 패키지를 이용하여 파싱(parsing)함
-
-# t.append(html.find_all(class_='hx'))
-
-# d.append(html.find_all(class_='time'))
-
-# l.append(html.find_all('a')) #href를 넣어서 쓰기
-
-# print(t)
-# print(d)
-# print(l)
-
+for i in time:
+    date=i.get_text()
+    print(date)
